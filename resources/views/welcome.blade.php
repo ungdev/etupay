c7156167821695987bc0f1244e13438a8a09ee76
Christian d'Autume <christian.dautume@gmail.com>
2016-08-17 14:44:05 +0200

    Title fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>EtuPay, la plateforme de paiment associative</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
