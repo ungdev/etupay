62cf4c9218d6d46fb53eb58f862bede5a6b0dd64
Christian d'Autume <christian.dautume@gmail.com>
2016-08-24 02:38:46 +0200

    Errors page modification

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -65,7 +65,7 @@
         </section>
     </div>
 </div>
-
+@include('layouts.footer')
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="/js/admin.min.js" type="text/javascript"></script>
