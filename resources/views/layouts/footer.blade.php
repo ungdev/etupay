d2d52b1194b5096fdff7f8e041efd291cc575db0
Christian d'Autume <christian.dautume@gmail.com>
2016-08-24 11:49:20 +0200

    Add piwik card

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,3 +5,20 @@
         <strong>En cas de problème,</strong> contacter <a href="mailto:christian+etupay@dautume.fr">Chris</a>.
     </div>
 </footer>
+
+<!-- Piwik -->
+<script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(["setDomains", ["*.etupay.utt.fr"]]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//piwik.uttnetgroup.fr/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '5']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+</script>
+<noscript><p><img src="//piwik.uttnetgroup.fr/piwik.php?idsite=5" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
