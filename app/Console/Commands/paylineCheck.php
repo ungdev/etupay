26f21de5438ebab962c71e8697ba73c73290bdf8
Christian d'Autume <christian@dautume.fr>
2019-03-05 20:21:58 +0100

    Add option to force reconciliation

--- app/Console/Commands/paylineCheck.php
+++ app/Console/Commands/paylineCheck.php
@@ -13,7 +13,7 @@ class paylineCheck extends Command
      *
      * @var string
      */
-    protected $signature = 'payline:check';
+    protected $signature = 'payline:check {--id=* : payline transaction id to force reconciliate}';
 
     /**
      * The console command description.
@@ -39,61 +39,87 @@ class paylineCheck extends Command
      */
     public function handle()
     {
-        $transactions = Transaction::where('step', 'INITIALISED')->get();
-        $this->info(count($transactions).' transaction initialisé.');
-        $this->info('Lancement de la tentative de résolution avec Payline');
-
+        $opt_id = $this->option('id');
         $paylineProvider = new PaylineProvider();
         $updated = 0;
 
-        $this->output->progressStart(count($transactions));
-        foreach ($transactions as $transaction)
+        if($opt_id)
         {
-            $tr = $paylineProvider->getTransaction($transaction->id);
-            if($tr)
+            foreach ($opt_id as $tr_id)
             {
-                $transaction->data = json_encode($tr);
-                $transaction->bank_transaction_id = $tr['transaction']['id'];
-                $transaction->provider = 'Payline';
-
-                switch ($tr['result']['code'])
+                $tr = $paylineProvider->getTransactionByPaylineId($tr_id);
+                if($tr)
                 {
-                    case '00000': // Accepted
-                    case '02400':
-                    case '02500':
-                    case '02501':
-                    case '02517':
-                    case '02520':
-                    case '02616':
-                    case '03000':
-                    case '04000':
-                        if($tr['payment']['amount'] != $transaction->amount)
-                        {
-                            throw new \Exception('Discordance in transaction amount');
-                            $transaction->save();
-                            $this->error('Discordance in transaction amount '.$transaction->id);
-                            return false;
-                        }
-                        $transaction->callbackAccepted();
-                        break;
-                    case '02324':
-                        //Transaction expiré
-                        $transaction->step = 'CANCELED';
-                        $transaction->save();
-                        break;
-                    default:
-                        $transaction->callbackRefused();
+                    $etupay_id = (explode('_', $tr['order']['ref']))[1];
+                    $transaction = Transaction::find($etupay_id);
+                    if ($transaction)
+                    {
+                        $this->info('Processing callback transaction ' . $transaction->id);
+                        $this->processTransaction($transaction, $tr);
+                        $updated++;
+                    } else {
+                        $this->error("Transaction not found ! ".$tr['order']['ref']." Payline: ".$tr_id);
+                    }
+                }
+            }
 
+        } else {
+            $transactions = Transaction::where('step', 'INITIALISED')->get();
+            $this->info(count($transactions) . ' transaction initialisé.');
+            $this->info('Lancement de la tentative de résolution avec Payline');
+
+            $this->output->progressStart(count($transactions));
+            foreach ($transactions as $transaction) {
+                $tr = $paylineProvider->getTransaction($transaction->id);
+                if ($tr) {
+                    $this->info('Processing callback transaction ' . $transaction->id);
+                    $this->processTransaction($transaction, $tr);
+                    $updated++;
                 }
 
-                $this->info('Processing callback transaction '.$transaction->id);
-                $transaction->save();
-                $updated++;
+                $this->output->progressAdvance();
             }
-            $this->output->progressAdvance();
+            $this->output->progressFinish();
         }
-        $this->output->progressFinish();
 
         $this->info('Nombre de transaction consilié: '.$updated);
+    }
+
+    private function processTransaction(Transaction $transaction, $tr)
+    {
+        $transaction->data = json_encode($tr);
+        $transaction->bank_transaction_id = $tr['transaction']['id'];
+        $transaction->provider = 'Payline';
+
+        switch ($tr['result']['code'])
+        {
+            case '00000': // Accepted
+            case '02400':
+            case '02500':
+            case '02501':
+            case '02517':
+            case '02520':
+            case '02616':
+            case '03000':
+            case '04000':
+                if($tr['payment']['amount'] != $transaction->amount)
+                {
+                    throw new \Exception('Discordance in transaction amount');
+                    $transaction->save();
+                    $this->error('Discordance in transaction amount '.$transaction->id);
+                    return false;
+                }
+                $transaction->callbackAccepted();
+                break;
+            case '02324':
+                //Transaction expiré
+                $transaction->step = 'CANCELED';
+                $transaction->save();
+                break;
+            default:
+                $transaction->callbackRefused();
+
+        }
+        $transaction->save();
     }
 }
