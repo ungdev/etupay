5ef1699d86de6925b5ab426b14f74cd10e1d3586
Christian d'Autume <christian@dautume.fr>
2019-05-28 15:43:32 +0200

    Route space cleaning

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,51 +9,51 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // CallBack ATOS
-Route::group(['middleware' => 'bank'], function(){
-    Route::get('/atos/callback', ['as'=> 'callback.atos', 'uses' => 'CallbackController@handleAtosCallback']);
-    Route::post('/atos/callback', ['as'=> 'callback.atos', 'uses' => 'CallbackController@handleAtosCallback']);
+Route::group(['middleware' => 'bank'], function () {
+    Route::get('/atos/callback', ['as' => 'callback.atos', 'uses' => 'CallbackController@handleAtosCallback']);
+    Route::post('/atos/callback', ['as' => 'callback.atos', 'uses' => 'CallbackController@handleAtosCallback']);
 });
 
-
-Route::group([], function(){
+Route::group([], function () {
     //Route::get('/transaction/{InitialisedTransaction}', ['as'=> 'userFrontend.choose', 'uses' => 'UserFrontend@paymentGatewayChoice']);
-    Route::get('/transaction/{uuid}', ['as'=> 'userFrontend.uuid.choose', 'uses' => 'UserFrontend@paymentGatewayChoice']);
+    Route::get('/transaction/{uuid}', ['as' => 'userFrontend.uuid.choose', 'uses' => 'UserFrontend@paymentGatewayChoice']);
 
-    Route::get('/atos/return', ['as'=> 'return.atos', 'uses' => 'UserFrontend@atosCallback']);
-    Route::post('/atos/return', ['as'=> 'return.atos', 'uses' => 'UserFrontend@atosCallback']);
+    Route::get('/atos/return', ['as' => 'return.atos', 'uses' => 'UserFrontend@atosCallback']);
+    Route::post('/atos/return', ['as' => 'return.atos', 'uses' => 'UserFrontend@atosCallback']);
 
     //Route::get('/paypal/return', ['as'=> 'return.paypal', 'uses' => 'UserFrontend@paypalCallback']);
 
-    Route::get('/paypal/return', ['as'=> 'return.paypal', 'uses' => 'UserFrontend@paypalCallback']);
-    Route::get('/transaction/{InitialisedTransaction}/paypal', ['as'=> 'userFrontend.paypalRedirect', 'uses' => 'UserFrontend@paypalRedirect']);
+    Route::get('/paypal/return', ['as' => 'return.paypal', 'uses' => 'UserFrontend@paypalCallback']);
+    Route::get('/transaction/{InitialisedTransaction}/paypal', ['as' => 'userFrontend.paypalRedirect', 'uses' => 'UserFrontend@paypalRedirect']);
 
     //Mode DEV:
-    Route::get('/transaction/devMode/{InitialisedTransaction}/{action}', ['as'=> 'userFrontend.devMode', 'uses' => 'UserFrontend@devAction']);
+    Route::get('/transaction/devMode/{InitialisedTransaction}/{action}', ['as' => 'userFrontend.devMode', 'uses' => 'UserFrontend@devAction']);
 
     //Payline
-    Route::get('/payline/return', ['as'=> 'return.payline', 'uses' => 'UserFrontend@paylineCallback']);
-    Route::get('/payline/callback', ['as'=> 'callback.payline', 'uses' => 'CallbackController@handlePaylineCallback']);
+    Route::get('/payline/return', ['as' => 'return.payline', 'uses' => 'UserFrontend@paylineCallback']);
+    Route::get('/payline/callback', ['as' => 'callback.payline', 'uses' => 'CallbackController@handlePaylineCallback']);
 });
 
-Route::group([], function(){
-    Route::get('/initiate', ['as'=> 'payment.request', 'uses' => 'TransactionRequest@incomming']);
-    Route::post('/initiate', ['as'=> 'payment.request', 'uses' => 'TransactionRequest@incomming']);
+Route::group([], function () {
+    Route::get('/initiate', ['as' => 'payment.request', 'uses' => 'TransactionRequest@incomming']);
+    Route::post('/initiate', ['as' => 'payment.request', 'uses' => 'TransactionRequest@incomming']);
 });
 
-
-
-Route::bind('InitialisedTransaction', function ($id){
+Route::bind('InitialisedTransaction', function ($id) {
     $transaction = \App\Models\Transaction::where('id', $id)->where('step', 'INITIALISED')->first();
-    if($transaction)
+    if ($transaction) {
         return $transaction;
-    else abort(404);
+    } else {
+        abort(404);
+    }
+
 });
 
 //Route::pattern('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
