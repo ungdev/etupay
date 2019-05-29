cefa5623567ad6da6cacf2131942c613edee065b
Christian d'Autume <christian@dautume.fr>
2019-05-29 14:50:07 +0200

    Add welcome page

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
  */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', ['as' => 'home', 'uses' => 'PageController@index']);
 
 // CallBack ATOS
 Route::group(['middleware' => 'bank'], function () {
@@ -45,6 +43,5 @@ Route::group([], function () {
     Route::get('/initiate', ['as' => 'payment.request', 'uses' => 'TransactionRequest@incomming']);
     Route::post('/initiate', ['as' => 'payment.request', 'uses' => 'TransactionRequest@incomming']);
 });
-
 
 //Route::pattern('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
