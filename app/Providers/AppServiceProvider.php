799d6730bd1102278c31495cf57bfff840ae82de
Christian d'Autume <christian@dautume.fr>
2019-07-25 09:33:40 +0200

    Remove telescope and upgrade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,9 +29,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->isLocal()) {
-            $this->app->register(TelescopeServiceProvider::class);
-        } else {
+        if (!$this->app->isLocal()) {
             URL::forceScheme('https');
         }
     }
